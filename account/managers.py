480492f9a91dd2f1a8987b8afe4c8ea91d6d68f0
banjialo <banji.alo@gmail.com>
2024-07-18 15:50:25 +1000

    created method for creating users

--- account/managers.py
+++ account/managers.py
@@ -5,4 +5,15 @@ from django.utils.translation import gettext_lazy as _
 
 #Define custom user class
 class CustomUserManager(BaseUserManager):
-    pass
+    
+    def create_user (self, email, password, **extra_fields):
+
+        if not email:
+
+            raise ValueError (_("The email must be set"))
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
