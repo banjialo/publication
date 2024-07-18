baadc435ff9a3c58bb228f55f247d5df2b2d4b35
banjialo <banji.alo@gmail.com>
2024-07-18 15:37:32 +1000

    Adding custom user manager

---
+++ account/managers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+from django.utils.translation import gettext_lazy as _
+
+
+#Define custom user class
+class CustomUserManager(BaseUserManager):
+
+    
