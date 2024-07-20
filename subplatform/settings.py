5d8a661d0df057a6b02fec8e7e8d3fe0f6c25da2
Banji Alo <banji.alo@gmail.com>
2024-07-20 19:52:36 +1000

    Added styling to register and login pages

--- subplatform/settings.py
+++ subplatform/settings.py
@@ -38,8 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+    
     'account',
 ]
+
+
+#initiatilise template
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
