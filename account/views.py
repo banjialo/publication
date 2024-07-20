e2eb0a2fda25517f4d501e72121c114f20b18922
banjialo <banji.alo@gmail.com>
2024-07-20 11:16:54 +1000

    Created registration page

--- account/views.py
+++ account/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . forms import CreateUserForm
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -6,7 +8,21 @@ def home(request):
 
 
 def register(request):
-    return render (request, 'account/register.html')
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('User registered!')
+    context = {'RegisterForm' : form}
+    return render (request, 'account/register.html', context)
+
+
+
+
+
+
 
 
 def my_login(request):
