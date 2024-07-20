7831c6b956cb6987172629f34b0812d32b91990e
banjialo <banji.alo@gmail.com>
2024-07-20 11:34:38 +1000

    Added login page

--- account/views.py
+++ account/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import CreateUserForm
 from django.http import HttpResponse
+
+#Import auth form from django
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
     return render (request, 'account/index.html')
 
-
+#create registration form
 def register(request):
     form = CreateUserForm()
 
@@ -14,16 +19,33 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse('User registered!')
+            return redirect ('my-login')
     context = {'RegisterForm' : form}
     return render (request, 'account/register.html', context)
 
 
 
 
-
-
-
-
 def my_login(request):
-    return render (request, 'account/my-login.html')
+    form = AuthenticationForm
+    
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username') #username / email
+            password = request.POST.get('password')
+
+            #username / email
+            user = authenticate(request, username=username, password=password)
+            if user is not None and user.is_writer == True:
+                login(request, user)
+                return HttpResponse('Welcome Writer')
+            
+            #if user is not a writer
+            if user is not None and user.is_writer == False:
+                login(request, user)
+                return HttpResponse('Welcome Client')
+            
+    context = {'LoginForm' : form}
+
+    return render (request, 'account/my-login.html', context)
