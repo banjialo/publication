09f6cea427f79d5d081f76c4f2573f66856fb7ba
Banji Alo <banji.alo@gmail.com>
2024-07-23 14:31:03 +1000

    Udpdate article complete

--- writer/views.py
+++ writer/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from . forms import ArticleForm
 from django.http import HttpResponse
+from . models import Article
 
 # Create your views here.
 
@@ -21,8 +22,46 @@ def create_article (request):
             article = form.save(commit=False)
             article.user = request.user
             article.save()
-            return HttpResponse ('Article Created')
+            return redirect ('my-articles')
 
     context = {'CreateArticleForm': form}
     
     return render (request, 'writer/create-article.html', context)
+
+
+@login_required(login_url='my-login')
+def my_articles(request):
+    
+    current_user = request.user.id
+    article = Article.objects.all().filter(user=current_user)
+    context = {'AllArticles' : article}
+    return render (request, 'writer/my-articles.html', context)
+
+
+
+@login_required(login_url='my-login')
+def update_article(request, pk):
+    
+    #retrieve the article ID
+    article = Article.objects.get(id=pk)
+    
+    #pass specific instance of the article
+    form = ArticleForm(instance=article)
+    
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        
+        #check if valid
+        if form.is_valid():
+            form.save()
+            return redirect ('my-articles')
+        
+    context = {'UpdateArticleForm' : form}    
+    
+    return render (request, 'writer/update-article.html', context)
+            
+    
+    
+    
+    
+    
