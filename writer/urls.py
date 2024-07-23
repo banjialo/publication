24f95d63a91570d3f063a5c80a8e0cab0d701f94
Banji Alo <banji.alo@gmail.com>
2024-07-23 13:40:12 +1000

    Create article feature

--- writer/urls.py
+++ writer/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     
-    path('writer-dashboard', views.writer_dashboard, name="writer-dashboard")
-    
+    path('writer-dashboard', views.writer_dashboard, name="writer-dashboard"),
+    path('create-article', views.create_article, name='create-article')
     
 ]
