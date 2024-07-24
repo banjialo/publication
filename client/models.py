8c70890ffa9820240e5e63414a5df6e1d25d38b1
Banji Alo <banji.alo@gmail.com>
2024-07-24 14:46:23 +1000

    Set-up subscription views

--- client/models.py
+++ client/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from account.models import CustomUser
+
 # Create your models here.
+
+class Subscription(models.Model):
+    
+    subscriber_name = models.CharField(max_length=300)
+    
+    subscription_plan = models.CharField(max_length=255)
+    
+    subscription_cost = models.CharField(max_length=255)
+    
+    paypal_subscription_id = models.CharField(max_length=300)
+    
+    is_active = models.BooleanField(default=False)
+    
+    user = models.OneToOneField(CustomUser, max_length=10, on_delete=models.CASCADE, unique = True) #one only user has only subscription
+    
+    def __str__(self):        
+        return f'{self.subscriber_name} - {self.subscription_plan} subscription'
+    
+    
+    
+    
+    
+    
--- client/views.py
+++ client/views.py
@@ -1,9 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required
+
+from writer.models import Article
+
+from .models import Subscription
 
 # Create your views here.
 
 @login_required(login_url='my-login')
 def client_dashboard (request):
-    return render(request, 'client/client-dashboard.html')
+    
+    try:
+        subDetails = Subscription.objects.get(user=request.user)
+        
+        Subscription_plan = subDetails.subscription_plan
+        
+        context = {'SubPlan' : Subscription_plan}
+        
+        return render(request, 'client/client-dashboard.html', context)
+    
+    except:
+        
+        Subscription_plan = "None"
+        
+        context = {'SubPlan' : Subscription_plan}
+    
+        return render(request, 'client/client-dashboard.html')
+
+
+@login_required(login_url='my-login')
+def browse_articles (request):
+    
+    try:        
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+        
+    except:
+        return render (request, 'client/subscription-locked.html')
+    
+    #id eevrything works fine in the try statement, do this:
+    current_subscription_plan = subDetails.subscription_plan
+    
+    if current_subscription_plan == 'Standard':
+        articles = Article.objects.all().filter(is_premium=False)
+        
+    elif current_subscription_plan == 'Premium':
+        articles = Article.objects.all ()
+        
+    context = {'AllClientArticles' : articles}
+    
+    return render (request, 'client/browse-articles.html', context )
+
+
+@login_required(login_url='my-login')
+def subscription_locked (request):
+    return render (request, 'client/subscription-locked.html')
+
